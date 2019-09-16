9b84d8c55871ee83b89ac163e043a27ae6acb19a
Khoi Le <le.khoi992@gmail.com>
2019-09-16 14:35:53 +0700

    add more create script

--- data.sql
+++ data.sql
@@ -1,5 +1,27 @@
 -- Table: public."employee"
 
+-- SEQUENCE: public.s1
+
+-- DROP SEQUENCE public.s1;
+
+CREATE SEQUENCE public.s1
+    INCREMENT 1
+    START 13
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+-- SEQUENCE: public.s2
+
+-- DROP SEQUENCE public.s2;
+
+CREATE SEQUENCE public.s2
+    INCREMENT 1
+    START 4
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
 -- DROP TABLE public."employee";
 
 CREATE TABLE public."employee"
@@ -7,6 +29,18 @@ CREATE TABLE public."employee"
     id bigint NOT NULL DEFAULT nextval('s1'::regclass),
     info jsonb,
     CONSTRAINT "employee_pkey" PRIMARY KEY (id)
+)
+
+-- DROP TABLE public.company;
+
+CREATE TABLE public.company
+(
+    id bigint NOT NULL DEFAULT nextval('s2'::regclass),
+    name character varying(100),
+    location character varying(100),
+    employees jsonb,
+    type character varying(100),
+    CONSTRAINT company_pkey PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
